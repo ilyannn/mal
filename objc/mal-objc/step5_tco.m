eecca7f3d078c9a7aa9ff886676d39b745fab89c
ilya n <ilya.nikokoshev@gmail.com>
2015-05-18 19:53:42 +0300

    use tco when eval

--- objc/mal-objc/step5_tco.m
+++ objc/mal-objc/step5_tco.m
@@ -64,71 +64,85 @@ NSString *PRINT(id ast) {
 
 - (id)eval:(id)ast env:(Environment *)env{
     
-    if (![ast isKindOfClass:[NSArray class]]) {
-        return [self eval_ast:ast env:env];
-    }
-
-    if ([[ast firstObject] isKindOfClass:[Symbol class]]) {
-        NSArray *specials = @[@"def!", @"let*", @"do", @"if", @"fn*"];
-        
-        __block __weak id wself = self;
-        
-        id def, first;
-        NSInteger index;
-        Environment *child;
-        NSArray *binds;
-        id expr;
-        
-        
-        switch ([specials indexOfObject:[[ast firstObject] name]]) {
-            case 0: // def!
-                def = [self eval:ast[2] env:env];
-                [env set:def forSymbol:ast[1]];
-                return def;
-                
-            case 1: // let*
-                child = [[Environment alloc] initWithOuter:env];
-                for (NSInteger index = 0; index < [ast[1] count];) {
-                    Symbol *symbol = ast[1][index++];
-                    id expr = ast[1][index++];
-                    [child set:[self eval:expr env:child] forSymbol:symbol];
-                }
-                return [self eval:ast[2] env:child];
-                
-            case 2: // do
-                return [[ast arrayByMapping:^id(id sub) {
-                    return [self eval_ast:sub env:env];
-                }] lastObject];
-                
-            case 3: // if
-                first = [self eval:ast[1] env:env];
-                index = [first truthValue] ? 2 : 3;
-                return [self eval:ast[index] env:env];
-                
-            case 4: // fn*
-                binds = ast[1];
-                expr = ast[2];
-                return [[Function alloc] initWithBody:^id(id args) {
-                    Environment *child = [[Environment alloc] initWithOuter:env 
-                                                                      binds:binds 
-                                                                      exprs:args];
-                    return [wself eval:expr env:child];
-                    
-                }];            
+    while (true) {
+        if (![ast isKindOfClass:[NSArray class]]) {
+            return [self eval_ast:ast env:env];
         }
         
+        if ([[ast firstObject] isKindOfClass:[Symbol class]]) {
+            NSArray *specials = @[@"def!", @"let*", @"do", @"if", @"fn*"];
+            
+            switch ([specials indexOfObject:[[ast firstObject] name]]) {
+                case 0: // def!
+                {
+                	id def = [self eval:ast[2] env:env];
+                    [env set:def forSymbol:ast[1]];
+                    return def;
+                }
+                    
+                case 1: // let*
+                {
+                    Environment *child = [[Environment alloc] initWithOuter:env];
+                    for (NSInteger index = 0; index < [ast[1] count];) {
+                        Symbol *symbol = ast[1][index++];
+                        id expr = ast[1][index++];
+                        [child set:[self eval:expr env:child] forSymbol:symbol];
+                    }
+                    env = child;
+                    ast = ast[2];
+                    continue; // tco
+                }
+                    
+                case 2: // do
+                {
+                    [ast enumerateObjectsUsingBlock:^(id sub, NSUInteger idx, BOOL *stop) {
+                        if (idx != [ast count]-1) {
+                            [self eval_ast:sub env:env];
+                        }
+                    }];
+                    
+                    ast = [ast lastObject];                    
+                    continue; // tco
+                }    
+
+                case 3: // if
+                {
+                    id first = [self eval:ast[1] env:env];
+                    NSInteger index = [first truthValue] ? 2 : 3;
+                    ast = ast[index];
+                    continue; // tco
+                }
+                    
+                case 4: // fn*
+                {
+                    __block __weak id wself = self;
+
+                    id binds = ast[1];
+                    id expr = ast[2];
+                    
+                    return [[Function alloc] initWithBody:^id(id args) {
+                        Environment *child = [[Environment alloc] initWithOuter:env 
+                                                                          binds:binds 
+                                                                          exprs:args];
+                        return [wself eval:expr env:child];
+                        
+                    }];            
+                }
+            }
+            
+        }
+        NSArray *evaluated = [self eval_ast:ast env:env];
+        
+        Function *op = [evaluated firstObject];
+        if (![op isKindOfClass:[Function class]]) {
+            @throw [NSException exceptionWithName:@"FunctionRequired" 
+                                           reason:@"Symbol that is a known function must be the first in a list" 
+                                         userInfo:nil];
+        }
+        
+        NSRange range = NSMakeRange(1, evaluated.count - 1);
+        return [op evaluateWithArguments:[evaluated subarrayWithRange:range]];
     }
-    NSArray *evaluated = [self eval_ast:ast env:env];
-    
-    Function *op = [evaluated firstObject];
-    if (![op isKindOfClass:[Function class]]) {
-        @throw [NSException exceptionWithName:@"FunctionRequired" 
-                                       reason:@"Symbol that is a known function must be the first in a list" 
-                                     userInfo:nil];
-    }
-    
-    NSRange range = NSMakeRange(1, evaluated.count - 1);
-    return [op evaluateWithArguments:[evaluated subarrayWithRange:range]];
 }
 
 - (NSString *)rep:(NSString *)line {
