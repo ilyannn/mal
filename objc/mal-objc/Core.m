6c4307ab13fc2b6bf4ec8fa4198042d2c2a16121
ilya n <ilya.nikokoshev@gmail.com>
2015-05-18 20:24:22 +0300

    add slurp

--- objc/mal-objc/Core.m
+++ objc/mal-objc/Core.m
@@ -13,6 +13,8 @@
 
 @implementation Core
 
+#pragma mark - Environment
+
 - (instancetype)init {
     if (self = [super init]) {
         _bindings = [[[self selectorDict] allKeys] // fix order
@@ -23,88 +25,30 @@
     return self;
 }
 
-- (NSNumber *)count:(NSArray *)args {
-    id first = [args firstObject];
-    if ([first isEqualTo:[NSNull null]]) {
-        return 0;
-    }
-    return @([first count]);
-}
-
-- (NSArray *)list:(NSArray *)args {
-    return args;
-}
-
-- (Truth *)listQ:(id)args {
-    id first = [args firstObject];
-    return [[Truth alloc] initWithTruth:[first isKindOfClass:[NSArray class]]];
-}
-
-- (Truth *)emptyQ:(id)args {
-    id first = [args firstObject];
-    return [[Truth alloc] initWithTruth: [first count] != 0];
-}
-
-- (Truth *)equals:(id)args {
-    id first = args[0];
-    id second = args[1];
-    return [[Truth alloc] initWithTruth: [first isEqualTo:second]];
-}
-
-- (NSInteger)add:(NSInteger)first to:(NSInteger)second {
-    return first + second;
-}
-
-- (NSInteger)subtractFrom:(NSInteger)first value:(NSInteger)second {
-    return first - second;
-}
-
-- (NSInteger)multiply:(NSInteger)first with:(NSInteger)second {
-    return first * second;
-}
-
-- (NSInteger)divide:(NSInteger)first by:(NSInteger)second {
-    return first / second;
-}
-
-- (BOOL)less:(NSInteger)first than:(NSInteger)second {
-    return first < second;
-}
-
-- (BOOL)lessEq:(NSInteger)first than:(NSInteger)second {
-    return first <= second;
-}
-
-- (BOOL)greater:(NSInteger)first than:(NSInteger)second {
-    return first > second;
-}
-
-- (BOOL)greaterEq:(NSInteger)first than:(NSInteger)second {
-    return first >= second;
-}
-
 - (NSDictionary *)selectorDict {
     return @{
-      @"count": @"count:", 
-       @"list": @"list:", 
-      @"list?": @"listQ:", 
-     @"empty?": @"emptyQ:", 
-          @"<": @"less:than:",
-          @">": @"greater:than:",
-          @"<=": @"lessEq:than:",
-          @">=": @"greaterEq:than:",
-          @"+": @"add:to:", 
-          @"-": @"subtractFrom:value:",
-          @"*": @"multiply:with:",
-          @"/": @"divide:by:", 
-          @"=": @"equals:"};
+             @"count": @"count:", 
+             @"list": @"list:", 
+             @"list?": @"listQ:", 
+             @"empty?": @"emptyQ:", 
+             @"<": @"less:than:",
+             @">": @"greater:than:",
+             @"<=": @"lessEq:than:",
+             @">=": @"greaterEq:than:",
+             @"+": @"add:to:", 
+             @"-": @"subtractFrom:value:",
+             @"*": @"multiply:with:",
+             @"/": @"divide:by:", 
+             @"=": @"equals:",
+             @"slurp": @"slurp:"
+             };
 }
 
 - (NSArray *)operations {
     return [[self bindings] arrayByMapping:^id(Symbol *sym) {        
         SEL sel = NSSelectorFromString([self selectorDict][sym.name]);
         NSMethodSignature *sign = [self methodSignatureForSelector:sel];
-	
+        
         BOOL numberOp = sign.numberOfArguments == 4;
         
         return [[Function alloc] initWithBody:^ id(id args) {
@@ -141,6 +85,92 @@
             }
         }];
     }];
+}
+
+
+#pragma mark - List functions
+
+- (NSNumber *)count:(NSArray *)args {
+    id first = [args firstObject];
+    if ([first isEqualTo:[NSNull null]]) {
+        return 0;
+    }
+    return @([first count]);
+}
+
+- (NSArray *)list:(NSArray *)args {
+    return args;
+}
+
+- (Truth *)listQ:(id)args {
+    id first = [args firstObject];
+    return [[Truth alloc] initWithTruth:[first isKindOfClass:[NSArray class]]];
+}
+
+- (Truth *)emptyQ:(id)args {
+    id first = [args firstObject];
+    return [[Truth alloc] initWithTruth: [first count] != 0];
+}
+
+- (Truth *)equals:(id)args {
+    id first = args[0];
+    id second = args[1];
+    return [[Truth alloc] initWithTruth: [first isEqualTo:second]];
+}
+
+#pragma mark - Integer functions
+
+- (NSInteger)add:(NSInteger)first to:(NSInteger)second {
+    return first + second;
+}
+
+- (NSInteger)subtractFrom:(NSInteger)first value:(NSInteger)second {
+    return first - second;
+}
+
+- (NSInteger)multiply:(NSInteger)first with:(NSInteger)second {
+    return first * second;
+}
+
+- (NSInteger)divide:(NSInteger)first by:(NSInteger)second {
+    return first / second;
+}
+
+
+- (BOOL)less:(NSInteger)first than:(NSInteger)second {
+    return first < second;
+}
+
+- (BOOL)lessEq:(NSInteger)first than:(NSInteger)second {
+    return first <= second;
+}
+
+- (BOOL)greater:(NSInteger)first than:(NSInteger)second {
+    return first > second;
+}
+
+- (BOOL)greaterEq:(NSInteger)first than:(NSInteger)second {
+    return first >= second;
+}
+
+#pragma mark - String and file functions
+
+- (NSString *)slurp:(id)args {
+    RequireElement(0, args, [NSString class]);                   
+    NSString *filename = [args firstObject];
+    
+    NSError *error;    
+    NSString *str = [NSString stringWithContentsOfFile:filename 
+                                              encoding:NSUTF8StringEncoding 
+                                                 error:&error];
+    
+    if (error) {
+        @throw [NSException exceptionWithName:@"FileAccessProblem" 
+                                       reason:@"Couldn't read a file" 
+                                     userInfo:@{@"error" : error}];
+    }
+    
+    return str;
 }
 
 @end
